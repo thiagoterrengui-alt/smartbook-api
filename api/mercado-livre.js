373722a032c47b9ac8d5c417c8319f927f2aacc8
thiagoterrengui-alt <thiago.terrengui@gmail.com>
2026-03-31 17:31:07 -0300

    Update mercado-livre.js

--- api/mercado-livre.js
+++ api/mercado-livre.js
@@ -9,23 +9,8 @@ export default async function handler(req, res) {
   }
 
   try {
-    const tokenRes = await fetch('https://api.mercadolibre.com/oauth/token', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: new URLSearchParams({
-        grant_type: 'client_credentials',
-        client_id: '7787620594905398',
-        client_secret: 'TZ5mFcrm1rHCFuAWCvO52P84cLXAoIXg'
-      })
-    })
-
-    const tokenData = await tokenRes.json()
-    const accessToken = tokenData.access_token
-
     const url = `https://api.mercadolibre.com/sites/MLB/search?q=${encodeURIComponent(q)}&limit=${limit}`
-    const response = await fetch(url, {
-      headers: { Authorization: `Bearer ${accessToken}` }
-    })
+    const response = await fetch(url)
     const data = await response.json()
     res.status(200).json(data)
   } catch (error) {
